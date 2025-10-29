297769dc4b92c790df0970e14c1110a00cffd74b
isaac <isaacformato@gmail.com>
2025-10-29 13:58:41 -0400

    no more compilation errors for running and stop at last

--- part3/elevator/elevator.c
+++ part3/elevator/elevator.c
@@ -2,18 +2,29 @@
 #include <linux/mutex.h>
 #include <linux/kthread.h>
 #include <linux/sched.h>
+#include <linux/slab.h>
+
+#define ENTRY_NAME "elevator"
+#define PERMS 0666
+#define PARENT NULL
+#define BUF_LEN 100
+
+static int __init elevator_init(void);
+static int __init elevator_exit(void);
+
+static int currPets;
+static int maxWeight;
+static int currFloor;
+static bool boolUp = 1;
+static bool stop = 0;
+
+static struct task_struct *elevator_thread;
+static struct task_struct *stop_thread;
 
 struct mutex mut;
-mutex_init(&mut);
+static int runningElevator(void *data);
+static int stopOperation(void *data);
 
-
-void stopOperation() {
-
-}
-
-void procRequest() {
-
-}
 
 struct list_head floorPets[5];
 struct list_head elevatorPets;
@@ -23,42 +34,38 @@ typedef struct pet {
     struct list_head list;
     int type;
     int destination;
-} pet
+} pet;
+
+int weightCheck(pet *p);
 
 
 // dumb way of getting weight based on using 'type' as just an int
 // theres better ways to do this, this is placeholder
-int weightCheck(pet) {
-    if (pet -> type == 0) {
+int weightCheck(pet *p) {
+    if (p -> type == 0) {
         return 3;
     }
-    if (pet -> type == 1) {
+    if (p -> type == 1) {
         return 14;
     }
-    if (pet -> type == 2) {
+    if (p -> type == 2) {
         return 10;
     }
     else {
-        return 16
+        return 16;
     }
 }
 
-struct task_struct * kthread_run(threadfin, data, namefmt);
+static int runningElevator(void *data) {
 
-
-
-void runningElevator() {
-    maxWeight = 0;
-    currFloor = 0;
-    currPets =0;
-    //temp scheduler
-    boolUp = true;
+    struct list_head *temp, *dummy;
+    pet * item;
     while (true) {
         mutex_lock(&mut);
         
         // unload first
         list_for_each_safe(temp, dummy, &elevatorPets) {
-            item = list_entry(temp, Item, list);
+            item = list_entry(temp, pet, list);
             if (item->destination == currFloor){
                 maxWeight -= weightCheck(item->type);
                 currPets -=1;
@@ -70,9 +77,9 @@ void runningElevator() {
         // move floors according to scheduling algorithm
         while (maxWeight < 50 && currPets < 5){
              list_for_each_safe(temp, dummy, &floorPets[currFloor]){
-                item = list_entry(temp, Item, &floorPets[currFloor]);
+                item = list_entry(temp, pet, list);
                 if (weightCheck(item->type) + maxWeight <= 50){
-                    list_add_tail(&item->left, &elevatorPets);
+                    list_add_tail(&item->list, &elevatorPets);
                     list_del(temp);
                     maxWeight += weightCheck(item->type);
                     currPets += 1;
@@ -81,13 +88,13 @@ void runningElevator() {
         }
         // temp scheduler before scheduling is actually made.
         if (currFloor == 4) {
-            boolUp = False;
+            boolUp = 0;
         }
         if (currFloor == 0)
         {
-            boolUp = true;
+            boolUp = 1;
         }
-        if (boolUp == true) {
+        if (boolUp == 1) {
             currFloor+=1;
         }
         else {
@@ -96,9 +103,66 @@ void runningElevator() {
         mutex_unlock(&mut);
 
     }
-
+    return 0;
 }
 
-void initailizeThreads() {
+
+int stopOperation() {
+    struct list_head *temp, *dummy;
+    pet * item;
+    while (true){
+        if (stop == 1) {
+            while (!list_empty(&elevatorPets)) {
+                mutex_lock(&mut);
+                list_for_each_safe(temp, dummy, &elevatorPets) {
+                    item = list_entry(temp, pet, list);
+                    if (item->destination == currFloor){
+                        maxWeight -= weightCheck(item->type);
+                        currPets -=1;
+                        list_del(temp);
+                        kfree(item);
+                    }
+                }
+
+
+            //in actuality, either follow scheduling algo
+            // or go directly to
+            if (currFloor == 4) {
+                boolUp = 0;
+            }
+            if (currFloor == 0)
+            {
+                boolUp = 1;
+            }
+            if (boolUp == 1) {
+                currFloor+=1;
+            }
+            else {
+                currFloor-=1;
+            }
+            mutex_unlock(&mut);
+        }
+    }
+}
+    return 0;
     
+}
+
+// void procRequest() {
+
+// }
+
+
+
+static int __init elevator_init(void) {
+    mutex_init(&mut);
+    printk(KERN_INFO, "setting up threads?");
+    elevator_thread = kthread_run(runningElevator, NULL, "elevator_thread");
+    stop_thread = kthread_run(stopOperation, NULL, "stop_thread");
+    return 0;
+}
+
+static void __exit elevator_exit() {
+    kthread_stop(elevator_thread);
+    kthread_stop(stop_thread);
 }
