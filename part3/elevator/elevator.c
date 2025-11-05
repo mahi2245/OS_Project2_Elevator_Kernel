73936e184c4a0d907563fd7dc09dbb797094e865
CardsChris89 <cferrer2023@gmail.com>
2025-11-05 06:53:55 -0500

    updates and reformats elevator.c for threads, linked lists, etc. to prevent deadlocks

--- part3/elevator/elevator.c
+++ part3/elevator/elevator.c
@@ -1,213 +1,309 @@
+#include <linux/init.h>
 #include <linux/list.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
 #include <linux/mutex.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
-#include<linux/module.h>
-#include<linux/delay.h>
+#include <linux/uaccess.h>
+#include <linux/delay.h>
+#include <linux/sched.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("OSPROJ2GROUP24");
-MODULE_DESCRIPTION("elevator");
+MODULE_AUTHOR("project2group14");
+MODULE_DESCRIPTION("Linux pet elevator module");
 MODULE_VERSION("1.0");
 
 #define ENTRY_NAME "elevator"
 #define PERMS 0666
 #define PARENT NULL
-#define BUF_LEN 100
+#define BUF_LEN 1000
+#define NUM_FLOORS 5
 
-static int __init elevator_init(void);
-static void __init elevator_exit(void);
-static void __init proc_init(void);
+enum Status {OFFLINE, IDLE, LOADING, UP, DOWN };
 
-static int currPets;
-static int maxWeight;
-static int currFloor;
-static bool boolUp = 1;
-static bool stop = 0;
-
-static struct task_struct *elevator_thread;
-static struct task_struct *stop_thread;
-static struct task_struct *proc_thread;
-
-struct mutex mut;
-static int runningElevator(void *data);
-static int stopOperation(void *data);
-static int procRequest(void *data);
-
-struct list_head floorPets[5];
-struct list_head elevatorPets;
-//not on a floor yet, just brought in from a request
-struct list_head waitingPets;
-
-// not sure yet
-typedef struct pet {
-    struct list_head list;
+typedef struct {
+    int goal;
     int type;
-    int destination;
-} pet;
+    int weight;
+    struct list_head node;
+} Pet;
+typedef struct {
+    enum Status status;
+    int current_floor;
+    int num_pets;
+    int weight;
+    int total;
+    int canpickup;
+    int target;
+    struct list_head pets;
+} Elevator;
+typedef struct {
+    int num_pets;
+    int floor_num;
+    struct list_head pets;
+    struct list_head node;
+} Floor;
+typedef struct {
+    struct list_head floors;
+} Building;
 
-int weightCheck(pet *p);
+static struct proc_dir_entry* proc_entry;
+static Elevator elevator;
+static Building building;
+static int procfs_buf_len;
+static char msg[BUF_LEN];
+extern int (*STUB_start_elevator)(void);
+extern int (*STUB_issue_request)(int, int, int);
+extern int (*STUB_stop_elevator)(void);
+static struct task_struct *elevator_thread;
+struct mutex mut;
 
 
-// dumb way of getting weight based on using 'type' as just an int
-// theres better ways to do this, this is placeholder
-int weightCheck(pet *p) {
-    if (p -> type == 0) {
-        return 3;
+int start_elevator(void);                                                           // starts the elevator to pick up and drop off passengers
+int issue_request(int start_floor, int destination_floor, int type);                // add passengers requests to specific floors
+int stop_elevator(void);                                                            // stops the elevator
+
+
+
+static ssize_t procfile_read(struct file* file, char* ubuf, size_t count, loff_t *ppos) {
+    int waiting_pets = 0;
+	mutex_lock(&mut);
+    //procfs_buf_len = snprintf(msg + procfs_buf_len, BUF_LEN, "current time: %lld.%9ld\nelapsed time: %lld.%9ld\n", time.tv_sec, time.tv_nsec, secs, nsecs);
+    procfs_buf_len = snprintf(msg + procfs_buf_len, BUF_LEN, "Elevator state: ");
+    switch (elevator.status) {
+        case OFFLINE:
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "OFFLINE\n");
+            break;
+        case IDLE:
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "IDLE\n");
+            break;
+        case UP:
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "UP\n");
+            break;
+        case DOWN:
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "DOWN\n");
+            break;
+        case LOADING:
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "LOADING\n");
+            break;
     }
-    if (p -> type == 1) {
-        return 14;
+    procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "Current floor: %d\nCurrent load: %d lbs\nElevator status: ", elevator.current_floor, elevator.weight);
+    Pet *p;
+    list_for_each_entry(p, &elevator.pets, node) {
+        switch (p->type) {
+            case 0:
+                procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "C");
+                break;
+            case 1:
+                procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "P");
+                break;
+            case 2:
+                procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "H");
+                break;
+            case 3:
+                procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "D");
+                break;
+        }
+        procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "%d ", p->goal);
     }
-    if (p -> type == 2) {
-        return 10;
+    procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "\n\n\n");
+    Floor *f;
+    list_for_each_entry(f, &building.floors, node) {
+        procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "[");
+        if (f->floor_num == elevator.current_floor) {
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "*");
+        } else {
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, " ");
+        }
+        procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "] Floor %d: %d ", f->floor_num, f->num_pets);
+        list_for_each_entry(p, &f->pets, node) {
+            switch (p->type) {
+                case 0:
+                    procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "C");
+                break;
+                case 1:
+                    procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "P");
+                break;
+                case 2:
+                    procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "H");
+                break;
+                case 3:
+                    procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "D");
+                break;
+            }
+            procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "%d ", p->goal);
+            waiting_pets++;
+        }
+        procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "\n");
     }
-    else {
-        return 16;
-    }
+    procfs_buf_len += snprintf(msg + procfs_buf_len, BUF_LEN, "\n\n\nNumber of pets: %d\nNumber of pets waiting: %d\nNumber of pets serviced: %d\n", elevator.num_pets, waiting_pets, elevator.total);
+	mutex_unlock(&mut);
+
+    if (*ppos > 0 || count < procfs_buf_len)
+        return 0;
+    if (copy_to_user(ubuf, msg, procfs_buf_len))
+        return -EFAULT;
+    *ppos = procfs_buf_len;
+    return procfs_buf_len;
 }
 
-static int runningElevator(void *data) {
 
-    struct list_head *temp, *dummy;
-    pet * item;
-    while (true) {
-        mutex_lock(&mut);
-        
-        // unload first
-        list_for_each_safe(temp, dummy, &elevatorPets) {
-            item = list_entry(temp, pet, list);
-            if (item->destination == currFloor){
-                maxWeight -= weightCheck(item->type);
-                currPets -=1;
-                list_del(temp);
-                kfree(item);
-                ssleep(1);
-            }
-        }
+static const struct proc_ops procfile_fops = {
+    .proc_read = procfile_read,
+};
 
-        // move floors according to scheduling algorithm
-        while (maxWeight < 50 && currPets < 5){
-             list_for_each_safe(temp, dummy, &floorPets[currFloor]){
-                item = list_entry(temp, pet, list);
-                if (weightCheck(item->type) + maxWeight <= 50){
-                    list_add_tail(&item->list, &elevatorPets);
-                    list_del(temp);
-                    maxWeight += weightCheck(item->type);
-                    currPets += 1;
-                    ssleep(1);
-                }
-             }
+int elevatorLoop(void* data) {
+    while(!kthread_should_stop()) {
+        while (elevator.status == OFFLINE && !kthread_should_stop()) {
+            printk("ELEVATOR PRE SLEEP OFFLINE");
+            msleep_interruptible(1000);
         }
-        // temp scheduler before scheduling is actually made.
-        if (currFloor == 4) {
-            boolUp = 0;
-        }
-        if (currFloor == 0)
-        {
-            boolUp = 1;
-        }
-        if (boolUp == 1) {
-            currFloor+=1;
-        }
-        else {
-            currFloor-=1;
-        }
-        ssleep(2);
-        mutex_unlock(&mut);
-
+        printk("ELEVATOR PRE SLEEP IDLE");
+        msleep_interruptible(2000);
+//        int waiting_pets = 0;
+//        Floor* f;
+//        Pet* p;
+//        list_for_each_entry(f, &building.floors, node) {
+//            list_for_each_entry(p, &f->pets, node) {
+//                waiting_pets++;
+//            }
+//        }
     }
+return 0;
+}
+
+int start_elevator(void) {
+    mutex_lock(&mut);
+    if (elevator.status == OFFLINE) {
+        wake_up_process(elevator_thread);
+        elevator.status = IDLE;
+    } else {
+        mutex_unlock(&mut);
+        return 1;
+    }
+    mutex_unlock(&mut);
     return 0;
 }
 
+int issue_request(int start_floor, int destination_floor, int type) {
+    if (start_floor > NUM_FLOORS || destination_floor > NUM_FLOORS || type < 0 || type > 3 || start_floor < 1 || destination_floor < 1)
+        return 1;
 
-static int stopOperation(void *data) {
-    struct list_head *temp, *dummy;
-    pet * item;
-    while (!kthread_should_stop()){
-        if (stop == 1) {
-            while (!list_empty(&elevatorPets)) {
-                mutex_lock(&mut);
-                list_for_each_safe(temp, dummy, &elevatorPets) {
-                    item = list_entry(temp, pet, list);
-                    if (item->destination == currFloor){
-                        maxWeight -= weightCheck(item->type);
-                        currPets -=1;
-                        list_del(temp);
-                        kfree(item);
-                        ssleep(1);
-                    }
-                }
-                mutex_unlock(&mut);
+    mutex_lock(&mut);
+    Floor* f;
+    list_for_each_entry(f, &building.floors, node) {
+        if (f->floor_num == start_floor) {
+            f->num_pets++;
+            Pet* newpet;
+            newpet = kzalloc(sizeof(*newpet), GFP_KERNEL);
+            if (!newpet)
+                return -ENOMEM;
+            newpet->goal = destination_floor;
+            newpet->type = type;
+            switch (type) {
+                case 0:
+                    newpet->weight = 3;
+                    break;
+                case 1:
+                    newpet->weight = 14;
+                    break;
+                case 2:
+                    newpet->weight = 10;
+                    break;
+                case 3:
+                    newpet->weight = 16;
+                    break;
+            }
+            list_add_tail(&newpet->node, &f->pets);
+        }
+    }
+    mutex_unlock(&mut);
+    return 0;
+}
 
-            mutex_lock(&mut);
-            //in actuality, either follow scheduling algo
-            // or go directly to
-            if (currFloor == 4) {
-                boolUp = 0;
-            }
-            if (currFloor == 0)
-            {
-                boolUp = 1;
-            }
-            if (boolUp == 1) {
-                currFloor+=1;
-            }
-            else {
-                currFloor-=1;
-            }
-            ssleep(2);
+int stop_elevator(void) {
+    if (elevator.canpickup == 0) return 1;
+    mutex_lock(&mut);
+    if (elevator.status != OFFLINE) {
+        while (list_count_nodes(&elevator.pets) > 0) {
+            elevator.canpickup = 0;
             mutex_unlock(&mut);
+            msleep(2000);
+            mutex_lock(&mut);
         }
+        elevator.status = OFFLINE;
     }
-}
+    mutex_unlock(&mut);
+
     return 0;
-    
 }
-
-
-
-static int procRequest(void *data) {
-    struct list_head *temp, *dummy;
-    pet * item;
-    while (!kthread_should_stop()){
-        // when request happens
-        // or while queue is occupied with pets
-        // should work through this queue, adding them onto floors
-        mutex_lock(&mut);
-        list_for_each_safe(temp, dummy, &waitingPets){
-            item = list_entry(temp, pet, list);
-            list_add_tail(&item->list, &floorPets[item->destination]);
-            list_del(temp);      
-        }
-        mutex_unlock(&mut);
-        
-    }
-    return 0;
-    
-}
-
-
 
 static int __init elevator_init(void) {
-    // set up lists 
-    INIT_LIST_HEAD(&elevatorPets);
-    INIT_LIST_HEAD(&waitingPets);
-    for (int i = 0; i < 5; i++){
-        INIT_LIST_HEAD(&floorPets[i]);
-    }
     mutex_init(&mut);
-    elevator_thread = kthread_run(runningElevator, NULL, "elevator_thread");
-    stop_thread = kthread_run(stopOperation, NULL, "stop_thread");
-    proc_thread = kthread_run(procRequest, NULL, "proc_thread");
-    return 0;
+    proc_entry = proc_create(ENTRY_NAME, PERMS, PARENT, &procfile_fops);
+    if (proc_entry == NULL)
+        return -ENOMEM;
+    INIT_LIST_HEAD(&building.floors);
+    Floor* t;
+    for (int i = 1; i <= NUM_FLOORS; i++) {
+        t = kzalloc(sizeof(*t), GFP_KERNEL);
+        if (!t)
+            return -ENOMEM;
+        t->num_pets = 0;
+        t->floor_num = i;
+        INIT_LIST_HEAD(&t->pets);
+        list_add(&t->node, &building.floors);
+    }
+    elevator.status = OFFLINE;
+    elevator.current_floor = 1;
+    elevator.num_pets = 0;
+    elevator.weight = 0;
+    elevator.total = 0;
+    elevator.canpickup = 1;
+    elevator.target = -1;
+    INIT_LIST_HEAD(&elevator.pets);
+    STUB_start_elevator = start_elevator;
+    STUB_issue_request = issue_request;
+    STUB_stop_elevator = stop_elevator;
+    elevator_thread = kthread_run(elevatorLoop, NULL, "elevator_thread");
+    if (IS_ERR(elevator_thread)) {
+        printk("elevator thread init error\n");
+        return PTR_ERR(elevator_thread);
+    }
+    return 0;  // Return 0 to indicate successful loading
 }
 
-static void __exit elevator_exit() {
+static void __exit elevator_exit(void) {
+    printk("ELEVATOR PRESTOP");
+    //stop_elevator();
+    printk("ELEVATOR POSTSTOP");
 
-    kthread_stop(elevator_thread);
-    kthread_stop(stop_thread);
-    kthread_stop(proc_thread);
+    printk("ELEVATOR PREKILL");
+    int ret = kthread_stop(elevator_thread);
+    if (ret != -EINTR)
+        printk("elevator thread stopped\n");
+    Floor *a, *b;
+    Pet *y, *z;
+    list_for_each_entry_safe(a, b, &building.floors, node) {
+        list_del(&a->node);
+        list_for_each_entry_safe(y, z, &a->pets, node) {
+            list_del(&y->node);
+            kvfree(y);
+        }
+        kvfree(a);
+    }
+
+    STUB_start_elevator = NULL;
+    STUB_issue_request = NULL;
+    STUB_stop_elevator = NULL;
+
+    proc_remove(proc_entry);
 }
 
-module_init(elevator_init);
-module_exit(elevator_exit);
+
+
+
+module_init(elevator_init);  // Specify the initialization function
+module_exit(elevator_exit);  // Specify the exit/cleanup function
